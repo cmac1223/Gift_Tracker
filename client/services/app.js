f5bde059e49c9f543eeca7dfb921bea9ae22d41f
cmac1223 <chrismac539@gmail.com>
2017-12-20 00:44:27 -0500

    app.js setup

--- client/services/app.js
+++ client/services/app.js
@@ -1,2 +1,7 @@
 require('angular-ui-router');
 require('angular-messages');
+const angular = require('angular');
+
+angular.module('myResolutionApp', ['ui.router', 'ngMessages']).config(uiRouterSetup);
+
+uiRouterSetup.$inject = ['$stateProvider', '$urlRouterProvider'];
