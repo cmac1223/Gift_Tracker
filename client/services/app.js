78a34740807c7afe9c4bda87ff59cc47bf2fcf6f
cmac1223 <chrismac539@gmail.com>
2017-12-20 00:53:30 -0500

    work on client side app.js

--- client/services/app.js
+++ client/services/app.js
@@ -5,3 +5,11 @@ const angular = require('angular');
 angular.module('myResolutionApp', ['ui.router', 'ngMessages']).config(uiRouterSetup);
 
 uiRouterSetup.$inject = ['$stateProvider', '$urlRouterProvider'];
+function uiRouterSetup($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('goals', {
+      url: '/goals',
+      template: '<goals></goals>'
+    })
+  $urlRouterProvider.otherwise('/');  
+}
