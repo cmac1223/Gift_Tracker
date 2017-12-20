71d1c3b7b295c908c44fc62ec67827d694059ec4
cmac1223 <chrismac539@gmail.com>
2017-12-19 20:00:04 -0500

    express setup

---
+++ db/schema.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Use native promises
+mongoose.Promise = global.Promise;
+
+var GoalSchema = new Schema({
+  Goal: { type: String, required: true },
+  Cost: { type: Number, required: true },
+  createAt: Date,
+  updatedAt: Date
+})
+
+GoalSchema.pre('save', function(next){
+  now = new Date();
+  this.updatedAt = now;
+
+  if( !this.createdAt ) {
+      this.createAt = now;
+  }
+  next();
+})
+
+var GoalModel = mongoose.model('Goal', GoalSchema);
+
+module.exports = {
+  Goal: GoalModel
+};
