7e5ab345234c586bb5bea909205218e38e661e00
cmac1223 <chrismac539@gmail.com>
2018-01-05 17:37:40 -0500

    added updateGoalInfo function along with success and failure promises followed by module.exports editGoalController

--- client/components/edit-goal/editGoal.controller.js
+++ client/components/edit-goal/editGoal.controller.js
@@ -19,6 +19,18 @@ function EditGoalController($state, $stateParams, GoalsService) {
   initialize();
 
   vm.updateGoalInformation = function(){
-    GoalsService.updateSingleGoal 
+    GoalsService.updateSingleGoal(
+      vm.goalToUpdate
+    ).then(
+      function success(response) {
+        // redirect to the individual credit page when successfully updated
+        $state.go('show_goal/:goalId', { goalId: vm.goalToUpdate._id });
+      },
+      function failure(response){
+        console.log('Failed to updated Goal with ID of ' + goalEntryId);
+      }
+    )
   }
 }
+
+module.exports = EditGoalController;
--- public/js/app.bundle.js
+++ public/js/app.bundle.js
@@ -45990,9 +45990,16 @@ function EditGoalController($state, $stateParams, GoalsService) {
   initialize();
 
   vm.updateGoalInformation = function () {
-    GoalsService.updateSingleGoal;
+    GoalsService.updateSingleGoal(vm.goalToUpdate).then(function success(response) {
+      // redirect to the individual credit page when successfully updated
+      $state.go('show_goal/:goalId', { goalId: vm.goalToUpdate._id });
+    }, function failure(response) {
+      console.log('Failed to updated Goal with ID of ' + goalEntryId);
+    });
   };
 }
+
+module.exports = EditGoalController;
 
 /***/ }),
 /* 84 */
