e2bb2905ce7a68d36283effd7b12ffdb152c44b2
cmac1223 <chrismac539@gmail.com>
2018-01-07 11:49:46 -0500

    find all shoppingsLists from DB

--- routes/shoppingLists.js
+++ routes/shoppingLists.js
@@ -6,6 +6,15 @@ var ShoppingList = require('../models/shoppingList');
 router.get('/', (request, response) => {
 
   //Find all of the Shopping-lists from the database
+  ShoppingList.find({}).exec(function (error, shoppingLists){
+    if (error) {
+      console.log('Error retrieving shoppingLists!');
+      console.log('Error: ' + error);
+      return;
+    }
+    // if there are no errors, send the shoppingLists back as JSON
+    response.send(shoppingLists);
+  })
 })
 
 module.exports = router;
